19d7acd67095292185a47489ecd7575bfc12755f
Justin Burger <him@wanderingburgers.com>
2016-06-12 21:04:28 -0700

    Basic formatting/cleanup

--- app.js
+++ app.js
@@ -2,24 +2,21 @@ var express = require('express'),
     bodyparser = require('body-parser'),
     util = require("util");
 
-
 var app = express();
-
 var port = process.env.PORT  || 5000;
-
 
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(bodyparser.json());
 
 
-var participantController = require('./controllers/participantController');
 
-participantRoutes =  require('./routes/participantRoutes')();
-inviteRoutes =  require('./routes/inviteRoutes')();
+var participantController = require('./controllers/participantController'),
+    participantRoutes =  require('./routes/participantRoutes')(),
+    inviteRoutes =  require('./routes/inviteRoutes')();
+
 
 app.use('/api/participant', participantRoutes);
 app.use('/api/invite', inviteRoutes);
-
 
 app.listen(port, function(err){
     console.log('running server on port ' + port);
--- routes/participantRoutes.js
+++ routes/participantRoutes.js
@@ -1,13 +1,12 @@
 var express = require('express');
-var routes = function(){
-    var participantRouter = express.Router();
-    var participant = require('../models/participantModel');
 
-    var participantController = require('../controllers/participantController')(participant);
+var routes = function(){
+    var participantRouter = express.Router(),
+        participant = require('../models/participantModel'),
+        participantController = require('../controllers/participantController')(participant);
 
     participantRouter.route('/:participantId')
         .get(participantController.get);
-
     return participantRouter;
 };
 
